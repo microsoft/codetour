29ddc9c2ebb124de2f71bb48d541b25d21897fbd
Sandeep Somavarapu <sasomava@microsoft.com>
2020-08-28 22:14:01 +0200

    prepare for web extension - use codicons instead of svgs and remove them - consolidate node and browser tasks into single webpack task - use browser end point - exclude unwanted files in .gitignore and .vscdeignore - fix task and launch configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,8 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
-  mode: "production",
-  target: "node",
+const config = {
   entry: "./src/extension.ts",
-  output: {
-    path: path.resolve(__dirname, "out"),
-    filename: "extension.js",
-    libraryTarget: "commonjs2",
-    devtoolModuleFilenameTemplate: "../[resource-path]"
-  },
   devtool: "source-map",
   externals: {
     vscode: "commonjs vscode"
@@ -44,3 +36,27 @@ module.exports = {
     })
   ]
 };
+
+const nodeConfig = {
+  ...config,
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension-node.js',
+    libraryTarget: "commonjs2",
+    devtoolModuleFilenameTemplate: "../[resource-path]",
+  }
+};
+
+const webConfig = {
+  ...config,
+  target: 'webworker',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'extension-web.js',
+    libraryTarget: "commonjs2",
+    devtoolModuleFilenameTemplate: "../[resource-path]",
+  }
+};
+
+module.exports = [nodeConfig, webConfig];
--- webpack.web.js
+++
@@ -1,6 +0,0 @@
-const devConfig = require("./webpack.config.js");
-const merge = require("webpack-merge");
-
-module.exports = merge(devConfig, {
-  target: "webworker"
-});

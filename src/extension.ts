50252c9f9e2a0ee715bcdeb61ef2667c08444c98
João Moreno <joao.moreno@microsoft.com>
2021-05-11 14:14:07 +0200

    add codetour._startTourOnActivation
    This command, when run right on startup, allows the default
    tour to begin immediately, instead of prompting the user.

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,13 @@
 
 import * as vscode from "vscode";
 import { initializeApi } from "./api";
+import { EXTENSION_NAME } from "./constants";
 import { initializeGitApi } from "./git";
 import { registerLiveShareModule } from "./liveShare";
 import { registerNotebookProvider } from "./notebook";
 import { registerPlayerModule } from "./player";
 import { registerRecorderModule } from "./recorder";
-import { promptForTour } from "./store/actions";
+import { promptForTour, startDefaultTour } from "./store/actions";
 import { discoverTours } from "./store/provider";
 
 export async function activate(context: vscode.ExtensionContext) {
@@ -17,9 +18,22 @@ export async function activate(context: vscode.ExtensionContext) {
   registerLiveShareModule();
   registerNotebookProvider();
 
+  let skipTourPrompt = false;
+  const skipTourPromptCommand = vscode.commands.registerCommand(
+    `${EXTENSION_NAME}._startTourOnActivation`,
+    () => skipTourPrompt = true
+  );
+
   if (vscode.workspace.workspaceFolders) {
     await discoverTours();
-    promptForTour(context.globalState);
+
+    skipTourPromptCommand.dispose();
+
+    if (skipTourPrompt) {
+      startDefaultTour();
+    } else {
+      promptForTour(context.globalState);
+    }
 
     initializeGitApi();
   }

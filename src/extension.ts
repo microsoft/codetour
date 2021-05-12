e706f96fe771c6dd3a741109bb6accfb1330eaec
João Moreno <joao.moreno@microsoft.com>
2021-05-12 11:31:30 +0200

    replace _startTourOnActivation with a URIHandler
    - handles `/startDefaultTour`

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,40 @@
 
 import * as vscode from "vscode";
 import { initializeApi } from "./api";
-import { EXTENSION_NAME } from "./constants";
 import { initializeGitApi } from "./git";
 import { registerLiveShareModule } from "./liveShare";
 import { registerNotebookProvider } from "./notebook";
 import { registerPlayerModule } from "./player";
 import { registerRecorderModule } from "./recorder";
 import { promptForTour, startDefaultTour } from "./store/actions";
-import { discoverTours } from "./store/provider";
+import { discoverTours as _discoverTours } from "./store/provider";
+
+/**
+ * In order to check whether the URI handler was called on activation,
+ * we must do this dance around `discoverTours`. The same call to
+ * `discoverTours` is shared between `activate` and the URI handler.
+ */
+let cachedDiscoverTours: Promise<void> | undefined;
+function discoverTours(): Promise<void> {
+  return cachedDiscoverTours ?? (cachedDiscoverTours = _discoverTours());
+}
+
+class URIHandler implements vscode.UriHandler {
+
+  private _didStartDefaultTour = false;
+  get didStartDefaultTour(): boolean { return this._didStartDefaultTour; }
+
+  async handleUri(uri: vscode.Uri): Promise<void> {
+    if (uri.path === '/startDefaultTour') {
+      this._didStartDefaultTour = true;
+
+      await discoverTours();
+      startDefaultTour();
+    }
+
+    return;
+  }
+}
 
 export async function activate(context: vscode.ExtensionContext) {
   registerPlayerModule(context);
@@ -18,20 +44,13 @@ export async function activate(context: vscode.ExtensionContext) {
   registerLiveShareModule();
   registerNotebookProvider();
 
-  let skipTourPrompt = false;
-  const skipTourPromptCommand = vscode.commands.registerCommand(
-    `${EXTENSION_NAME}._startTourOnActivation`,
-    () => skipTourPrompt = true
-  );
+  const uriHandler = new URIHandler();
+  context.subscriptions.push(vscode.window.registerUriHandler(uriHandler));
 
   if (vscode.workspace.workspaceFolders) {
     await discoverTours();
 
-    skipTourPromptCommand.dispose();
-
-    if (skipTourPrompt) {
-      startDefaultTour();
-    } else {
+    if (!uriHandler.didStartDefaultTour) {
       promptForTour(context.globalState);
     }
 
